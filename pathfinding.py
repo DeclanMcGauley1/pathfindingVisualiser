d7b89bade868b87ac66f3de274d8e79364f27710
DeclanMcGauley <73233273+DeclanMcGauley1@users.noreply.github.com>
2020-12-21 07:16:23 +0000

    back where I was but did a complete reformat of the entire code

--- pathfinding.py
+++ pathfinding.py
@@ -1,149 +1,100 @@
 import pygame
-import random
-import math
 import time
+import math
 
-#create the window
 WIDTH = 800
-WINDOW = pygame.display.set_mode((WIDTH, WIDTH))
 ROWS = 50
 GAP = WIDTH // ROWS
-pygame.display.set_caption("Pathfinding visualiser")
+WINDOW = pygame.display.set_mode((WIDTH, WIDTH))
+pygame.display.set_caption("pathfinding")
+
+WHITE = (255,255,255)
+BLACK = (0,0,0)
+BLUE = (0, 0, 210)
+RED = (255, 0, 0)
+
 
 class Node:
-
-    def __init__(self, row, col, width, totalRows):
+    def __init__(self, colour, row, col):
+        self.colour = colour
         self.row = row
         self.col = col
-        self.width = width
-        self.totalRows = totalRows
-        self.neighbors = []
-        self.x = row * width
-        self.y = col * width
-        self.visited = False
-        self.barrior = False
-        self.start = False
-        self.end = False
-        self.colour = (255,255,255)
+        self.x = row * GAP
+        self.y = col * GAP
 
-    def getPosition(self):
-        return self.row, self.col
-
-    def isVisited(self):
-        return self.visited
-
-    def isBarrior(self):
-        return self.barrior
-
-    def isStart(self):
-        return self.start
-
-    def isEnd(self):
-        return self.end
-
-    def setBarrior(self):
-        self.barrior = bool
-        self.colour = (0,0,0)
-
-    def setVisited(self):
-        self.visited = True
-        self.colour = (255,0,0)
-
-    def setStart(self):
-        self.start = True
-        self.colour = (0,0,200)
-
-    def setEnd(self):
-        self.end = True
-        self.colour = (0,150,150)
-
-    def clearBarrior(self):
-        self.barrior = False
-        self.colour = (255,255,255)
+    def setColour(self, colour):
+        self.colour = colour
 
     def draw(self, window):
-        pygame.draw.rect(window, self.colour, (self.x, self.y, self.width, self.width))
+        pygame.draw.rect(window, self.colour, (self.x, self.y, GAP, GAP))
 
-def eucDistance(pointA, pointB):
-    x1, y1 = pointA
-    x2, y2 = pointB
-
-    xDist = abs(x2 - x1)
-    yDist = abs(y2 - y1)
-
-    eucDist = math.sqrt((xDist ** 2) + (yDist ** 2))
-    print(eucDist)
-
-def makeGrid(rows, width):
-    grid = []
-    for row in range(rows):
+def Grid():
+    grid = list()
+    for row in range(ROWS):
         grid.append([])
-        for col in range(rows):
-            node = Node(row, col, GAP, rows)
+        for col in range(ROWS):
+            node = Node(WHITE, row, col)
             grid[row].append(node)
-
     return grid
 
-def drawGrid(window, width, rows):
-    for row in range(rows):
-        pygame.draw.line(window, (220,220,220), (0, row * GAP), (width, row * GAP))
-        for col in range(rows):
-            pygame.draw.line(window, (220, 220, 220), (col * GAP, 0), (col*GAP, width))
+def drawGrid():
+    for row in range(ROWS):
+        pygame.draw.line(WINDOW, (100, 100, 100), (0, row * GAP), (WIDTH, row*GAP))
+        for col in range(ROWS):
+            pygame.draw.line(WINDOW, (100,100,100), (col * GAP, 0), (col*GAP, WIDTH))
 
-
-def draw(window, grid, rows, width):
-    window.fill((0,0,0))
+def draw(grid):
+    WINDOW.fill((0,0,0))
     for row in grid:
         for node in row:
-            node.draw(window)
+            node.draw(WINDOW)
 
-    drawGrid(window, width, rows)
+    drawGrid()
     pygame.display.update()
 
+def getIndex(pos):
+    x, y = pos
+    row = x // GAP
+    col = y // GAP
+    return (row, col)
+
 def main():
+    grid = Grid()
     run = True
     startSet = False
     endSet = False
-    grid = makeGrid(50, WIDTH)
+
     while run:
-        draw(WINDOW, grid, 50, WIDTH)
-
+        draw(grid)
         for event in pygame.event.get():
-            if pygame.mouse.get_pressed()[0]:
-                pos = pygame.mouse.get_pos()
-                x, y = pos
-                xRow = x // GAP
-                yRow = y // GAP
-                grid[xRow][yRow].setBarrior()
-
-            if pygame.mouse.get_pressed()[2]:
-                pos = pygame.mouse.get_pos()
-                x, y = pos
-                xRow = x // GAP
-                yRow = y // GAP
-                grid[xRow][yRow].clearBarrior()
-
             if event.type == pygame.QUIT:
                 run = False
-
-        keys = pygame.key.get_pressed()
-        if keys[pygame.K_s]:
-            if startSet == False:
+            if pygame.mouse.get_pressed()[0]:
                 pos = pygame.mouse.get_pos()
-                x, y = pos
-                xRow = x // GAP
-                yRow = y // GAP
-                if grid[xRow][yRow].isEnd() == False:
-                    grid[xRow][yRow].setStart()
+                row, col = getIndex(pos)
+                grid[row][col].setColour(BLACK)
+            if pygame.mouse.get_pressed()[2]:
+                pos = pygame.mouse.get_pos()
+                row, col = getIndex(pos)
+                if grid[row][col].colour == RED:
+                    endSet = False
+                elif grid[row][col].colour == BLUE:
+                    startSet = False
+                grid[row][col].setColour(WHITE)
+
+
+            keys = pygame.key.get_pressed()
+            if keys[pygame.K_s]:
+                if not startSet:
+                    pos = pygame.mouse.get_pos()
+                    row, col = getIndex(pos)
+                    grid[row][col].setColour(BLUE)
                     startSet = True
-
-        if keys[pygame.K_e]:
-            if endSet == False:
-                pos = pygame.mouse.get_pos()
-                x, y = pos
-                xRow = x // GAP
-                yRow = y // GAP
-                if grid[xRow][yRow].isStart() == False:
-                    grid[xRow][yRow].setEnd()
+            if keys[pygame.K_e]:
+                if not endSet:
+                    pos = pygame.mouse.get_pos()
+                    row, col = getIndex(pos)
+                    grid[row][col].setColour(RED)
                     endSet = True
+
 main()
